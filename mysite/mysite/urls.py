982b241bbb47faa912533c8a325cfebcc61d2301
adi160 <araj21686@gmail.com>
2019-03-25 11:56:02 +0530

    final commit
    feel free to suggest for improvement

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.contrib.auth import views 
+from django.urls import path,re_path,include
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
-    path('accounts/login/$', views.login, name='login'),
-    path('accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('blog.urls')),
+    re_path(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    re_path(r'^accounts/logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
